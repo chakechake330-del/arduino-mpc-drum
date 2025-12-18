06b85c273af09a0a702ab6a1e50e5b12a317f731
M_Saito <chakechake330@gmail.com>
2025-12-19 01:28:32 +0900

    Update ArduinoIDE.ino

--- code/ArduinoIDE.ino
+++ code/ArduinoIDE.ino
@@ -1,21 +1,15 @@
 #include <FastLED.h>
 
-#define NUM_LEDS           // LEDの数（Processingのbandsと合わせる）
-#define DATA_PIN 6            // LEDのデータピン
+#define NUM_LEDS 256
+#define DATA_PIN 6
 
-#define TRIG_PIN 9            // 超音波センサーのTRIGピン
-#define ECHO_PIN 10           // 超音波センサーのECHOピン
+#define TRIG_PIN 9
+#define ECHO_PIN 10
 
 CRGB leds[NUM_LEDS];
 
 String inputString = "";
 bool receiving = false;
-
-unsigned long lastNoteTime = 0;
-bool noteOnSent = false;
-byte currentNote = 0;
-
-long lastDistance = 0;  // 前回の距離を記録
 
 void setup() {
   Serial.begin(115200);
@@ -28,11 +22,27 @@ void setup() {
 }
 
 void loop() {
-  handleLEDSerial();
-  handleUltrasonicMIDI();
+  sendDistance();     // 超音波センサーの値を送信
+  receiveLEDData();   // ProcessingからのLEDデータを受信
+  delay(30);
 }
 
-void handleLEDSerial() {
+void sendDistance() {
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  long duration = pulseIn(ECHO_PIN, HIGH, 20000);
+  float distance = duration * 0.034 / 2.0;
+
+  if (distance > 2 && distance < 400) {
+    Serial.println(distance);  // Processingに送信
+  }
+}
+
+void receiveLEDData() {
   while (Serial.available()) {
     char inChar = (char)Serial.read();
     if (inChar == '\n') {
@@ -64,43 +74,7 @@ void processInput(String data) {
   }
 
   for (int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CHSV(i * 8, 255, values[i]);  // 色相を細かく分けてカラフルに
+    leds[i] = CHSV(160, 255, values[i]);  // 色は青系、必要に応じて変更
   }
   FastLED.show();
-}
-
-void handleUltrasonicMIDI() {
-  long duration, distance;
-
-  digitalWrite(TRIG_PIN, LOW);
-  delayMicroseconds(2);
-  digitalWrite(TRIG_PIN, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG_PIN, LOW);
-
-  duration = pulseIn(ECHO_PIN, HIGH, 20000); // タイムアウト20ms
-  distance = duration * 0.034 / 2;
-
-  // 0cmが出たら前回の値を使う
-  if (distance == 0) {
-    distance = lastDistance;
-  } else {
-    lastDistance = distance;
-  }
-
-  if (distance > 5 && distance < 50 && !noteOnSent) {
-    currentNote = map(distance, 5, 50, 72, 48);  // C5〜C3
-    Serial.write((byte)(0x90));  // Note On
-    Serial.write(currentNote);
-    Serial.write((byte)100);
-    noteOnSent = true;
-    lastNoteTime = millis();
-  }
-
-  if (noteOnSent && millis() - lastNoteTime > 300) {
-    Serial.write((byte)(0x80));  // Note Off
-    Serial.write(currentNote);
-    Serial.write((byte)0);
-    noteOnSent = false;
-  }
 }
